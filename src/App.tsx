b64be047af77edfe3fc8a4729f10ed24d1c2bbe0
Yousef Shanawany <askyous@gmail.com>
2022-06-21 14:50:44 -0700

    Authorization works

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import './App.css';
 import { LocalStorageInput } from "./LocalStorageInput";
 
 function App() {
-    const [url, setUrl] = useState<string>();
+    const [oktaDomain, setOktaDomain] = useState<string>();
+    const [clientId, setClientId] = useState<string>();
+
+    const authUrl = useMemo(() => {
+        if (!clientId) return;
+        try {
+            const url = new URL(`${oktaDomain}/oauth2/v1/authorize`);
+            url.searchParams.set("redirect_uri", window.location.href);
+            url.searchParams.set("client_id", clientId);
+            url.searchParams.set("response_type", "token id_token");
+            url.searchParams.set("scope", "openid");
+            url.searchParams.set("state", "some-state");
+            url.searchParams.set("nonce", "some-nonce");
+            return url.toString();
+        }
+        catch (e) { return; }
+    }, [oktaDomain, clientId]);
 
     return <div>
-        <form>
-            <LocalStorageInput placeholder="https://example.okta.com" key="url" onChange={setUrl} />
-            <LocalStorageInput placeholder="Client ID" key="client-id" onChange={setUrl} />
+        <form onSubmit={(e) => {
+            e.preventDefault();
+            if (!authUrl) return;
+            window.open(authUrl);
+        }}>
+            <LocalStorageInput placeholder="https://example.okta.com" lsKey="okta-domain" onChange={setOktaDomain} />
+            <LocalStorageInput placeholder="Client ID" lsKey="client-id" onChange={setClientId} />
+            <pre>{authUrl}</pre>
             <button type="submit">Authenticate</button>
         </form>
     </div>;
